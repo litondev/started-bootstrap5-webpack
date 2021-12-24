21c6df3c52c364baad4b706241be5b51730f827c
litoninot12345@gmail.com <litoninot12345@gmail.com>
2021-12-24 21:09:47 +0700

    add sweetalert

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,14 @@ import '../scss/bootstrap.scss'; // SCSS
 
 // Load Main Scss
 import '../scss/main.scss';
+
+const Swal = require('sweetalert2')
+
+document.getElementById("klikMe").addEventListener("click",function(){
+    Swal.fire({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+    })    
+});
